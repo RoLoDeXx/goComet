a520d21c8551cf728848f1bd82b0e6f2d76dd16c
Samarth Sharma <dexxrolo@gmail.com>
2021-05-31 06:27:48 +0530

    added preface

--- src/components/Listings/index.js
+++ src/components/Listings/index.js
@@ -79,7 +79,59 @@ const Listings = () => {
           </div>
         </div>
       ) : (
-        "Type something in search box to start searching"
+        <div>
+          <h1>Not myntra</h1>
+          <p>
+            Following is my attempt at myntra challenge by goComet. Challenge
+            consisted of creating a product portal where user can search for
+            items, filter the results, sort the results and store them in a
+            virtual bag. Other portions of challenge included implenting
+            responsive architecture to support cross device support.
+          </p>
+          <h2>Instructrions</h2>
+          <p>
+            Type in the search box to anything you desire, the search results
+            are generated using fakerit api, no query would matter. The filters
+            and sorting mechanisms however would work completely fine. Filtering
+            and sorting of elements are calculated based on randomly fetched
+            data using a hastable to store frequent keywords, price ranges and
+            categories are however constants. Sorting is achieved by
+            Array.sort() over the JSON array.
+          </p>
+          <h2>Tech Stack</h2>
+          <ul>
+            <li>React</li>
+            <li>Pure CSS</li>
+            <li>React Gallery image slider</li>
+            <li>Fakerit api</li>
+          </ul>
+          <h2>Things I could've been better at</h2>
+          <ul>
+            <li>
+              I overestimated my momentum, submitting this 7 hours post deadline
+            </li>
+            <li>
+              Would've been better if fonts, logos and other assets were
+              implemented
+            </li>
+            <li>Mobile interface design could be much more neat</li>
+          </ul>
+          <h2>Time taken ~22 hours</h2>
+          <p>
+            I worked in three different phases to complete the challenge. They
+            are:{" "}
+          </p>
+          <ul>
+            <li>Saturday, 29 May, 6pm - 12pm</li>
+            <li>Sunday, 30 May, 12:30pm - 10pm</li>
+            <li>Monday, 31 May, 12am - 6:27am</li>
+          </ul>
+          Signed <br />
+          <br />
+          <b>Samarth Sharma</b>
+          <br />
+          6:27am, 5/31/2021
+        </div>
       )}
     </div>
   );
